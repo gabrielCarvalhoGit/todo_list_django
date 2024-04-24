0c4341dc3c0c0980e593866c3692cc21111b43f1
gabrielCarvalhoGit <gabriel.carvalhogss@gmail.com>
2024-04-24 16:26:11 -0300

    update version crispy-bootstrap

--- build_files.sh
+++ build_files.sh
@@ -2,4 +2,4 @@ python3.9 -m ensurepip
 
 python3.9 -m pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic
+python3.9 manage.py collectstatic --noinput
